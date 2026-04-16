5ce3f8c173921fb234763be79ba60b02623440f4
vennro <vennro@gmail.com>
2026-04-16 22:51:10 +0700

    save

--- jullybride.ru/wp-content/themes/tailpress-theme/resources/js/app.js
+++ jullybride.ru/wp-content/themes/tailpress-theme/resources/js/app.js
@@ -1,33 +1,11 @@
 window.addEventListener('load', function () {
-    const burger = document.getElementById('jb-burger-btn')
-    const close = document.getElementById('jb-close-menu')
-    const menu = document.getElementById('jb-mobile-menu')
+    let mainNavigation = document.getElementById('primary-navigation')
+    let mainNavigationToggle = document.getElementById('primary-menu-toggle')
 
-    if (burger && close && menu) {
-        const openMenu = function () {
-            menu.classList.remove('hidden')
-            window.setTimeout(function () {
-                menu.classList.remove('translate-x-full')
-            }, 10)
-            document.body.style.overflow = 'hidden'
-            burger.setAttribute('aria-expanded', 'true')
-        }
-
-        const closeMenu = function () {
-            menu.classList.add('translate-x-full')
-            burger.setAttribute('aria-expanded', 'false')
-            document.body.style.overflow = ''
-            window.setTimeout(function () {
-                menu.classList.add('hidden')
-            }, 300)
-        }
-
-        burger.addEventListener('click', function () {
-            openMenu()
-        })
-
-        close.addEventListener('click', function () {
-            closeMenu()
+    if(mainNavigation && mainNavigationToggle) {
+        mainNavigationToggle.addEventListener('click', function (e) {
+            e.preventDefault()
+            mainNavigation.classList.toggle('hidden')
         })
     }
 })
